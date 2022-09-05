e5bab7bbb95c1a5626cf22125a4ce11fd45648b2
hyun0721 <hyunee721@gmail.com>
2022-09-05 23:22:49 +0900

    * Live Server 및 Chrome Debugger 셋팅 * 최소값 예제 풀이

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 import oddNumber from "./1. basic/oddNumber.js";
-import * as FormHandler from "./999. etc/ex_formData.js";
+import min from "./1. basic/min.js";
 
 (function () {
     console.log(" >>>>  main.js  <<<< ");
 
-    // 1.
-    oddNumber();
+    // 1. 세 수 중 최솟값
+    console.log(`Answer ... ${min()}`);
+    
+    
+
+
 
 })();
