9eb22c124d4ced1123412526c108081ea682abec
hyun0721 <hyunee721@gmail.com>
2022-09-18 21:29:41 +0900

    라이브러리 추가

---
+++ 999. etc/ex_promise.js
@@ -0,0 +1,37 @@
+export default function(){
+
+
+    console.log('Start Promise Excercise ...');
+
+    var formData = new FormData();
+
+    
+
+
+
+    
+
+    // $.ajax({
+    //     url: "http://localhost:8080/temp",
+    //     type: 'post',
+    //     data: formData,
+    //     cache: false,
+    //     contentType: false,
+    //     processData : false,
+    //     // enctype: 'multipart/form-data',
+    //     error: function(jqXHR, status, error){
+
+    //         console.log('fail...');
+    //         console.log(jqXHR);
+    //         console.log(status);
+    //         console.log(error);
+            
+    //     },
+    //     success: function(data, jqXHR, textStatus){
+
+    //         console.log('success...');
+            
+    //     }
+    // });
+
+}
--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-import oddNumber from "./1. basic/oddNumber.js";
-import min from "./1. basic/min.js";
-import triangle from "./1. basic/triangle.js";
+import oddNumber    from "./1. basic/oddNumber.js";
+import min          from "./1. basic/min.js";
+import triangle     from "./1. basic/triangle.js";
+import ex_promise   from "./999. etc/ex_promise.js";
 
 (function () {
     console.log(" >>>>  main.js  <<<< ");
 
     // 1. 세 수 중 최솟값
-    console.log(`Answer ... ${triangle(2,3,4)}`);
-    
-    
+    // console.log(`Answer ... ${triangle(2,3,4)}`);
 
-
+    ex_promise();
 
 })();
