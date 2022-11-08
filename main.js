083e52d698a14b35b380d0720a9ca9d77fa2f338
hyun0721 <hyunee721@gmail.com>
2022-11-08 23:40:09 +0900

    *  1부터 N까지의 합

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import oddNumber    from "./1. basic/oddNumber.js";
 import min          from "./1. basic/min.js";
 import triangle     from "./1. basic/triangle.js";
 import pencilCount  from "./1. basic/pencilCount.js";
+import sum          from "./1. basic/sum.js";
 
 /* Excercise */
 import ex_promise   from "./999. etc/ex_promise.js";
@@ -11,5 +12,5 @@ import { SessionUtil, LocalUtil } from "./999. etc/ex_userStorage.js"
 (function () {
     console.log(" >>>>  main.js  <<<< ");
 
-    console.log(`Answer ... : ` + pencilCount(8));
+    console.log('Answer... : ' + sum(10));
 })();
