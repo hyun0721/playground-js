a93bcaba120564f59d3d5b1ddad4d5dfa8f6e10c
hyun0721 <hyunee721@gmail.com>
2022-11-08 23:32:55 +0900

    * 연필개수문제 (올림)

--- main.js
+++ main.js
@@ -1,31 +1,15 @@
+/** Algorism */
 import oddNumber    from "./1. basic/oddNumber.js";
 import min          from "./1. basic/min.js";
 import triangle     from "./1. basic/triangle.js";
+import pencilCount  from "./1. basic/pencilCount.js";
 
+/* Excercise */
 import ex_promise   from "./999. etc/ex_promise.js";
-
 import { SessionUtil, LocalUtil } from "./999. etc/ex_userStorage.js"
 
 (function () {
     console.log(" >>>>  main.js  <<<< ");
-    
-    SessionUtil.Clear();
-    LocalUtil.Clear();
 
-    SessionUtil.Insert('A' , '111');
-    SessionUtil.Insert('B' , '222');
-    SessionUtil.Insert('C' , '3333');
-
-    let sessionStorageAll = SessionUtil.GetAll();
-
-
-    LocalUtil.Insert('a' , '__11');
-    LocalUtil.Insert('b' , '__22');
-    LocalUtil.Insert('c' , '__33');
-
-    let localStorageAll = LocalUtil.GetAll();
-
-
-    debugger;
-
+    console.log(`Answer ... : ` + pencilCount(8));
 })();
