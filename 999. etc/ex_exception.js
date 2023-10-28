eaa07df91f9b91620e386647882bcb3dabdb9663
donghyun <hyunee721@gmail.com>
2023-10-28 22:00:26 +0900

    * Custom Error 예제

--- 999. etc/ex_exception.js
+++ 999. etc/ex_exception.js
@@ -1,10 +1,8 @@
 const DEFAULT_REGEXP_ALPHABET = /^[a-zA-Z]+$/;        // 영문자(알파벳)
 const DEFAULT_REGEXP_ALPHANUM = /^[0-9a-zA-Z]+$/;     // 영문자 + 숫자
 const DEFAULT_REGEXP_FIGURE = /^[0-9]+$/;             // 숫자
+const DEFAULT_REGEXP_DATE = /^(19[7-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;  // 날짜
 
-/**
- * 오류 클래스 명칭과 메시지를 상위객체에 반영
- */
 class BaseError extends Error {
     constructor(message) {
         super(message);
@@ -12,7 +10,7 @@ class BaseError extends Error {
     }
 }
 
-class ValidationError extends BaseError {
+class FormatError extends BaseError {
     constructor(message, seq, property) {
         super(message);
         this.seq = seq;
@@ -20,24 +18,26 @@ class ValidationError extends BaseError {
     }
 }
 
-class LogicalError extends ValidationError {
-    // constructor(message, seq, property, code){
-                
-    // }
+class LogicalError extends BaseError {
+    constructor(message, seq, property, category){
+        super(message);
+        this.seq = seq;
+        this.property = property;
+        this.category = category;
+    }
 }
 
+class APISendError extends BaseError {
+    constructor(message, url, code){
+        super(message);
+        this.url = url;
+        this.code = code;
+    }
+}
 
-const VadlidationForSave = (sourceData) => {
-
-    const DateHandler = new Intl.DateTimeFormat();
-
-    for(let data of sourceData){
-        if(!(DEFAULT_REGEXP_ALPHABET).test(data.product)){
-            throw new ValidationError(`입력유형 오류 : 영문자(알파벳)`, data.seq, 'product' );
-        }
-        // if(!(DEFAULT_REGEXP_ALPHABET).test(data.product)){
-
-        // }
+class SaveError extends BaseError {
+    constructor(message){
+        super(message);
     }
 }
 
@@ -50,37 +50,97 @@ const VadlidationForSave = (sourceData) => {
  *  2) 품절 여부 검사 : 저장 품목 중 품절 상품이 있으면 안됨.
  * 
  * 2. 저장
+ * 
  */
 export default () => {
 
-    const sourceData = [
-        { seq: '001', product: 'apple', price: '1000', expireDt: '2023-12-31', soldOut: false},
-        { seq: '002', product: 'banaa', price: '2000', expireDt: '2024-01-01', soldOut: false},
-        { seq: '003', product: 'melon123', price: '4000', expireDt: '2033-10-01', soldOut: true},
-        { seq: '004', product: 'grape', price: '1500', expireDt: '2023-11-15', soldOut: false},
-    ];
+    /**
+     * 1. 유효성 검사
+     * 
+     * @param {*} rowData
+     */
+    const Validation = (rowData) => {
 
-    // 1. Validation
-    try{
+        // 1. 자료형에 대한 유효성 검사
+        // 2. 로직상의 유효성 검사 (품절 여부 등)
 
-        VadlidationForSave(sourceData);
-
-    }catch(e){
-        if(e instanceof ValidationError){
-            console.log(`[${e.name}] :: ${e.message} - seq: ${e.seq}, property: ${e.property}`);
+        if(!(DEFAULT_REGEXP_ALPHABET).test(rowData.product)){
+            throw new FormatError(`자료유형 오류 : 영문자(알파벳)`, rowData.seq, rowData.product );
         }
-        else{
-            throw e;
+
+        if(!(DEFAULT_REGEXP_FIGURE).test(rowData.price)){
+            throw new FormatError(`자료유형 오류 : 숫자`, rowData.seq, rowData.price );
         }
+
+        if(!(DEFAULT_REGEXP_DATE).test(rowData.expireDt)){
+            throw new FormatError(`자료유형 오류 : 날짜`, rowData.seq, rowData.expireDt);
+        }
+
+        if(rowData.soldOut){
+            const category = `STOCK`;
+            throw new LogicalError(`논리유형 오류`, rowData.seq, rowData.soldOut ? '품절' : '재고', category);
+        }
+
     }
 
-    // 2. Save
-    // axios.post(`${DATA_SAVE_API}`)
-    //     .then((response) => {
+    /**
+     * 2. 데이터 전송 부
+     */
+    const SendData = (sendData) => {
+        
+        const DATA_SAVE_API = 'http://test.com';
 
-    //     })
-    //     .catch((error) => {
-    //         throw error;
-    //     })
+        axios.post(`${DATA_SAVE_API}`, {params: sendData})
+            .then((response) => {
+
+                /** Success Process */
+
+                if(response.state = 'invalid'){
+                    throw new APISendError('유효하지 않은 응답', DATA_SAVE_API, response.code);
+                }
+            })
+            .catch((error) => {
+                throw new APISendError('전송 실패', DATA_SAVE_API, '');
+            })
+
+    }
+
+    const SaveData = (sourceData) => {
+
+        /** 1. 유효성 검사 */
+        try{
+            sourceData.map(row => Validation(row));
+        }
+        catch(error) {
+            if(error instanceof FormatError){ throw new SaveError(`[${error.message}]\n행(SEQ): ${error.seq}\n입력값: ${error.property}`); }
+            else if (error instanceof LogicalError) { throw new SaveError(`[${error.message}]\n행(SEQ): ${error.seq}\n입력값: ${error.property}\n구분: ${error.category}`); }
+            else { throw error; }
+        }
+
+        /** 2. API Send (Save) */
+        try{
+            SendData(sourceData);
+        }
+        catch(error){
+            if(error instanceof APISendError){ throw new SaveError(`[API Error]\nCode: ${error.code}\nMessage: ${error.message}`); }
+        }
+    }
+    
+
+    try{
+        const sourceData = [
+            { seq: '001', product: 'apple', price: '1000', expireDt: '2023-12-31', soldOut: false},
+            { seq: '002', product: 'banaa', price: '2000', expireDt: '2024-01-00', soldOut: false},
+            // { seq: '003', product: 'melon123', price: '4000', expireDt: '2033-10-01', soldOut: true},
+            { seq: '004', product: 'grape', price: '1500', expireDt: '2023-11-15', soldOut: false},
+            { seq: '005', product: 'cherry', price: '4400', expireDt: '2023-11-15', soldOut: true},
+        ];
+
+        SaveData(sourceData);
+
+    }catch(e){
+        if(e instanceof SaveError) { alert(e.message); }
+        else { throw e; }
+    }
 
 }
